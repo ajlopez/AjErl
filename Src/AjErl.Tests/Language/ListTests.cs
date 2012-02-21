354c78cacb583623f336e210e09dfee8a15ac53a
ajlopez <webmaster@ajlopez.com>
2012-02-21 16:10:24 -0300

    List with first test

---
+++ Src/AjErl.Tests/Language/ListTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjErl.Language;
+
+namespace AjErl.Tests.Language
+{
+    [TestClass]
+    public class ListTests
+    {
+        [TestMethod]
+        public void CreateSimpleList()
+        {
+            List list = new List(1, 2);
+
+            Assert.AreEqual(1, list.Head);
+            Assert.AreEqual(2, list.Tail);
+        }
+    }
+}
---
+++ Src/AjErl/Language/List.cs
@@ -0,0 +1,23 @@
+﻿namespace AjErl.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class List
+    {
+        private object head;
+        private object tail;
+
+        public List(object head, object tail)
+        {
+            this.head = head;
+            this.tail = tail;
+        }
+
+        public object Head { get { return this.head; } }
+
+        public object Tail { get { return this.tail; } }
+    }
+}
