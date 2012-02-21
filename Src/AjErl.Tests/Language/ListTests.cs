4785f087153193324bb304f2d47b9d6538e6e98e
ajlopez <webmaster@ajlopez.com>
2012-02-21 19:09:27 -0300

    More List match tests

--- Src/AjErl.Tests/Language/ListTests.cs
+++ Src/AjErl.Tests/Language/ListTests.cs
@@ -49,6 +49,28 @@ namespace AjErl.Tests.Language
         }
 
         [TestMethod]
+        public void NoMatchWithDifferentHeads()
+        {
+            List list = new List(0, 2);
+            List list2 = new List(1, 2);
+            Context context = new Context();
+
+            Assert.IsNull(list.Match(list2, context));
+            Assert.IsNull(list2.Match(list, context));
+        }
+
+        [TestMethod]
+        public void NoMatchWithDifferentTails()
+        {
+            List list = new List(1, 0);
+            List list2 = new List(1, 2);
+            Context context = new Context();
+
+            Assert.IsNull(list.Match(list2, context));
+            Assert.IsNull(list2.Match(list, context));
+        }
+
+        [TestMethod]
         public void MatchTwoListsWithThreeElements()
         {
             List list = new List(1, new List(2, 3));
@@ -68,6 +90,48 @@ namespace AjErl.Tests.Language
 
             Assert.IsNull(list.Match(list2, context));
             Assert.IsNull(list2.Match(list, context));
+        }
+
+        [TestMethod]
+        public void MatchWithAVariable()
+        {
+            List list = new List(1, new Variable("X"));
+            List list2 = new List(1, new List(2, 3));
+            Context context = new Context();
+
+            var result = list.Match(list2, context);
+
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(context, result);
+            Assert.AreEqual(list2.Tail, result.GetValue("X"));
+
+            var result2 = list2.Match(list, context);
+
+            Assert.IsNotNull(result2);
+            Assert.AreNotEqual(context, result2);
+            Assert.AreEqual(list2.Tail, result2.GetValue("X"));
+        }
+
+        [TestMethod]
+        public void MatchHeadAndTailWithVariables()
+        {
+            List list = new List(new Variable("H"), new Variable("T"));
+            List list2 = new List(1, new List(2, 3));
+            Context context = new Context();
+
+            var result = list.Match(list2, context);
+
+            Assert.IsNotNull(result);
+            Assert.AreNotEqual(context, result);
+            Assert.AreEqual(list2.Head, result.GetValue("H"));
+            Assert.AreEqual(list2.Tail, result.GetValue("T"));
+
+            var result2 = list2.Match(list, context);
+
+            Assert.IsNotNull(result2);
+            Assert.AreNotEqual(context, result2);
+            Assert.AreEqual(list2.Head, result2.GetValue("H"));
+            Assert.AreEqual(list2.Tail, result2.GetValue("T"));
         }
     }
 }
