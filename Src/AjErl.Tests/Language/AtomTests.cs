c0cc58544680c9f5964046542c74ff1ff9954115
ajlopez <webmaster@ajlopez.com>
2012-02-21 16:27:27 -0300

    Atom with first test

---
+++ Src/AjErl.Tests/Language/AtomTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AjErl.Language;
+
+namespace AjErl.Tests.Language
+{
+    [TestClass]
+    public class AtomTests
+    {
+        [TestMethod]
+        public void CreateSimpleAtom()
+        {
+            Atom atom = new Atom("one");
+
+            Assert.AreEqual("one", atom.Name);
+        }
+    }
+}
---
+++ Src/AjErl/Language/Atom.cs
@@ -0,0 +1,19 @@
+﻿namespace AjErl.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Atom
+    {
+        private string name;
+
+        public Atom(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name { get { return this.name; } }
+    }
+}
