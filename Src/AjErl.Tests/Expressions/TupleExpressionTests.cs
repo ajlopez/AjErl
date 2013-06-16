215b8db4cf48f64717eefb43e0c1c92074f20fb6
ajlopez <webmaster@ajlopez.com>
2013-06-16 16:33:48 -0300

    Tuple Expression first tests

---
+++ Src/AjErl.Tests/Expressions/TupleExpressionTests.cs
@@ -0,0 +1,54 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Language;
+
+    [TestClass]
+    public class TupleExpressionTests
+    {
+        [TestMethod]
+        public void CreateEmptyTuple()
+        {
+            var expr = new TupleExpression(new IExpression[] { });
+
+            Assert.IsFalse(expr.HasVariable());
+
+            var result = expr.Evaluate(null);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Tuple));
+
+            var tuple = (Tuple)result;
+
+            Assert.AreEqual(0, tuple.Arity);
+        }
+
+        [TestMethod]
+        public void CreateSimpleTuple()
+        {
+            Context context = new Context();
+            var expr = new TupleExpression(new IExpression[] { new ConstantExpression(1), new VariableExpression(new Variable("X")), new AtomExpression(new Atom("y"))});
+
+            Assert.IsTrue(expr.HasVariable());
+
+            var result = expr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Tuple));
+
+            var tuple = (Tuple)result;
+
+            Assert.AreEqual(3, tuple.Arity);
+            Assert.AreEqual(1, tuple.ElementAt(0));
+            Assert.IsInstanceOfType(tuple.ElementAt(1), typeof(Variable));
+            Assert.AreEqual("X", ((Variable)tuple.ElementAt(1)).Name);
+            Assert.IsInstanceOfType(tuple.ElementAt(2), typeof(Atom));
+            Assert.AreEqual("y", ((Atom)tuple.ElementAt(2)).Name);
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/TupleExpression.cs
@@ -0,0 +1,37 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class TupleExpression : IExpression
+    {
+        private IList<IExpression> expressions;
+
+        public TupleExpression(IEnumerable<IExpression> expressions)
+        {
+            this.expressions = new System.Collections.Generic.List<IExpression>(expressions);
+        }
+
+        public object Evaluate(Context context)
+        {
+            IList<object> elements = new List<object>();
+
+            foreach (var expr in this.expressions)
+                elements.Add(expr.Evaluate(context));
+
+            return new Tuple(elements);
+        }
+
+        public bool HasVariable()
+        {
+            foreach (var expr in this.expressions)
+                if (expr.HasVariable())
+                    return true;
+
+            return false;
+        }
+    }
+}
