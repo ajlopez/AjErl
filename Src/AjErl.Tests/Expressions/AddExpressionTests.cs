944e86166da8a4de7762968c0fbebcb3e14ac67b
ajlopez <webmaster@ajlopez.com>
2013-12-08 17:44:30 -0300

    AddExpression, first tests

---
+++ Src/AjErl.Tests/Expressions/AddExpressionTests.cs
@@ -0,0 +1,45 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AddExpressionTests
+    {
+        [TestMethod]
+        public void AddTwoIntegers()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(1), new ConstantExpression(2));
+
+            Assert.AreEqual(3, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddIntegerToDouble()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(1), new ConstantExpression(2.5));
+
+            Assert.AreEqual(1 + 2.5, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddDoubleToInteger()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(2.5), new ConstantExpression(1));
+
+            Assert.AreEqual(2.5 + 1, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void AddTwoDoubles()
+        {
+            AddExpression expr = new AddExpression(new ConstantExpression(2.5), new ConstantExpression(3.7));
+
+            Assert.AreEqual(2.5 + 3.7, expr.Evaluate(null));
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/AddExpression.cs
@@ -0,0 +1,28 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class AddExpression : BinaryExpression
+    {
+        public AddExpression(IExpression left, IExpression right)
+            : base(left, right)
+        {
+        }
+
+        public override object Apply(object leftvalue, object rightvalue)
+        {
+            if (leftvalue is int)
+                if (rightvalue is int)
+                    return (int)leftvalue + (int)rightvalue;
+                else
+                    return (int)leftvalue + (double)rightvalue;
+            else if (rightvalue is int)
+                return (double)leftvalue + (int)rightvalue;
+            else
+                return (double)leftvalue + (double)rightvalue;
+        }
+    }
+}
