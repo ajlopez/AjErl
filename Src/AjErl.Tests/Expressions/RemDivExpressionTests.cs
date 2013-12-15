5966cf05fd911c8bc0c21c723a29c96929cb9157
ajlopez <webmaster@ajlopez.com>
2013-12-15 18:55:40 -0300

    Rem Expression with first tests

---
+++ Src/AjErl.Tests/Expressions/RemDivExpressionTests.cs
@@ -0,0 +1,61 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RemExpressionTests
+    {
+        [TestMethod]
+        public void RemainderOfTwoIntegers()
+        {
+            RemExpression expr = new RemExpression(new ConstantExpression(6), new ConstantExpression(2));
+
+            Assert.AreEqual(0, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void NonZeroRemainderOfTwoIntegers()
+        {
+            RemExpression expr = new RemExpression(new ConstantExpression(5), new ConstantExpression(2));
+
+            Assert.AreEqual(1, expr.Evaluate(null));
+        }
+
+        [TestMethod]
+        public void RaiseIfRemainderIntegerByReal()
+        {
+            RemExpression expr = new RemExpression(new ConstantExpression(6), new ConstantExpression(2.5));
+
+            try
+            {
+                expr.Evaluate(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidCastException));
+            }
+        }
+
+        [TestMethod]
+        public void RaiseIfRemainderRealByInteger()
+        {
+            RemExpression expr = new RemExpression(new ConstantExpression(6.5), new ConstantExpression(2));
+
+            try
+            {
+                expr.Evaluate(null);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidCastException));
+            }
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/RemExpression.cs
@@ -0,0 +1,20 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class RemExpression : BinaryExpression
+    {
+        public RemExpression(IExpression left, IExpression right)
+            : base(left, right)
+        {
+        }
+
+        public override object Apply(object leftvalue, object rightvalue)
+        {
+            return (int)leftvalue % (int)rightvalue;
+        }
+    }
+}
