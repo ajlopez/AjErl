289f97166b7b1135549549e166c3c8de6993618f
ajlopez <webmaster@ajlopez.com>
2013-12-23 20:10:02 -0300

    Function first test, evaluate constant body wo/arguments

---
+++ Src/AjErl.Tests/Language/FunctionTests.cs
@@ -0,0 +1,22 @@
+﻿namespace AjErl.Tests.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+    using AjErl.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class FunctionTests
+    {
+        [TestMethod]
+        public void EvaluateConstantBody()
+        {
+            Function function = new Function(new ConstantExpression(1));
+
+            Assert.AreEqual(1, function.Evaluate(null));
+        }
+    }
+}
---
+++ Src/AjErl/Language/Function.cs
@@ -0,0 +1,23 @@
+﻿namespace AjErl.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+
+    public class Function
+    {
+        private IExpression body;
+
+        public Function(IExpression body)
+        {
+            this.body = body;
+        }
+
+        public object Evaluate(Context context)
+        {
+            return this.body.Evaluate(context);
+        }
+    }
+}
