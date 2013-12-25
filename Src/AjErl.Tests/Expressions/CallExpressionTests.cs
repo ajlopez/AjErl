8d224fba66bc3357ef0a557d83281a018c9fb51a
ajlopez <webmaster@ajlopez.com>
2013-12-25 20:32:00 -0300

    CallExpression first test

---
+++ Src/AjErl.Tests/Expressions/CallExpressionTests.cs
@@ -0,0 +1,31 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Language;
+    using AjErl.Expressions;
+
+    [TestClass]
+    public class CallExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateCallExpression()
+        {
+            Context context = new Context();
+            Function function = new Function(context, new object[] { new Variable("X"), new Variable("Y") }, new AddExpression(new VariableExpression(new Variable("X")), new VariableExpression(new Variable("Y"))));
+            context.SetValue("add", function);
+            CallExpression expr = new CallExpression(new AtomExpression(new Atom("add")), new IExpression[] { new ConstantExpression(1), new ConstantExpression(2) });
+
+            Assert.IsFalse(expr.HasVariable());
+            Assert.IsNotNull(expr.NameExpression);
+            Assert.IsInstanceOfType(expr.NameExpression, typeof(AtomExpression));
+            Assert.IsNotNull(expr.ArgumentExpressions);
+            Assert.AreEqual(2, expr.ArgumentExpressions.Count);
+
+            Assert.AreEqual(3, expr.Evaluate(context));
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/CallExpression.cs
@@ -0,0 +1,56 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class CallExpression : IExpression
+    {
+        private IExpression nameexpression;
+        private IList<IExpression> argumentexpressions;
+
+        public CallExpression(IExpression nameexpression, IList<IExpression> argumentexpressions)
+        {
+            this.nameexpression = nameexpression;
+            this.argumentexpressions = argumentexpressions;
+        }
+
+        public IExpression NameExpression { get { return this.nameexpression; } }
+
+        public IList<IExpression> ArgumentExpressions { get { return this.argumentexpressions; } }
+
+        public object Evaluate(Context context, bool withvars = false)
+        {
+            object namevalue = this.nameexpression.Evaluate(context, withvars);
+            string name = null;
+
+            IList<object> arguments = new List<object>();
+
+            foreach (var argexpr in this.argumentexpressions)
+                arguments.Add(argexpr.Evaluate(context, withvars));
+
+            if (namevalue is Atom)
+                namevalue = context.GetValue(((Atom)namevalue).Name);
+
+            Function func = (Function)namevalue;
+
+            var newcontext = func.MakeContext(arguments);
+
+            return func.Evaluate(newcontext);
+        }
+
+        public bool HasVariable()
+        {
+            if (this.nameexpression.HasVariable())
+                return true;
+
+            foreach (var expr in this.argumentexpressions)
+                if (expr.HasVariable())
+                    return true;
+
+            return false;
+        }
+    }
+}
