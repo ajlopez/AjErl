00c4718dd4d36e39a7ed6401c232eee724596565
ajlopez <webmaster@ajlopez.com>
2013-12-26 18:55:05 -0300

    Machine first simple test

---
+++ Src/AjErl.Tests/MachineTests.cs
@@ -0,0 +1,20 @@
+﻿namespace AjErl.Tests
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MachineTests
+    {
+        [TestMethod]
+        public void CreateMachineWithRootContext()
+        {
+            Machine machine = new Machine();
+
+            Assert.IsNotNull(machine.RootContext);
+        }
+    }
+}
---
+++ Src/AjErl/Machine.cs
@@ -0,0 +1,19 @@
+﻿namespace AjErl
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Machine
+    {
+        private Context rootcontext;
+
+        public Machine()
+        {
+            this.rootcontext = new Context();
+        }
+
+        public Context RootContext { get { return this.rootcontext; } }
+    }
+}
