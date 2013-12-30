e2fb8dde194fc4af292a9167ccaebd5bbd2b501b
ajlopez <webmaster@ajlopez.com>
2013-12-30 20:13:09 -0300

    CompileModuleFunction first test

---
+++ Src/AjErl.Tests/Functions/CompileModuleFunctionTests.cs
@@ -0,0 +1,42 @@
+﻿namespace AjErl.Tests.Functions
+{
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Functions;
+    using AjErl.Language;
+
+    [TestClass]
+    public class CompileModuleFunctionTests
+    {
+        [TestMethod]
+        [DeploymentItem("Modules\\arith.erl")]
+        public void CompileArithModule()
+        {
+            Machine machine = new Machine();
+            CompileModuleFunction func = new CompileModuleFunction(machine);
+
+            var result = func.Apply(new object[] { new Atom("arith") });
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Tuple));
+
+            var tuple = (Tuple)result;
+
+            Assert.AreEqual(2, tuple.Arity);
+
+            var elem1 = tuple.ElementAt(0);
+
+            Assert.IsNotNull(elem1);
+            Assert.IsInstanceOfType(elem1, typeof(Atom));
+            Assert.AreEqual("ok", ((Atom)elem1).Name);
+
+            var elem2 = tuple.ElementAt(1);
+
+            Assert.IsNotNull(elem2);
+            Assert.IsInstanceOfType(elem2, typeof(Atom));
+            Assert.AreEqual("arith", ((Atom)elem2).Name);
+        }
+    }
+}
---
+++ Src/AjErl/Functions/CompileModuleFunction.cs
@@ -0,0 +1,26 @@
+﻿namespace AjErl.Functions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class CompileModuleFunction : IFunction
+    {
+        private Machine machine;
+
+        public CompileModuleFunction(Machine machine)
+        {
+            this.machine = machine;
+        }
+
+        public object Apply(IList<object> arguments)
+        {
+            Atom atom = (Atom)arguments[0];
+            this.machine.LoadModule(atom.Name);
+            Tuple tuple = new Tuple(new object[] { new Atom("ok"), atom });
+            return tuple;
+        }
+    }
+}
