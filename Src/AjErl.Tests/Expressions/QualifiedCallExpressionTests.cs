23bc7345e1732afbfcf662cf0c01ac5fb261f912
ajlopez <webmaster@ajlopez.com>
2014-01-01 19:40:27 -0300

    Qualified call expression with first test

---
+++ Src/AjErl.Tests/Expressions/QualifiedCallExpressionTests.cs
@@ -0,0 +1,35 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Expressions;
+    using AjErl.Language;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class QualifiedCallExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateQualifiedCallExpression()
+        {
+            Context context = new Context();
+            Module module = new Module(null);
+            context.SetValue("mod", module);
+            Function function = new Function(context, new object[] { new Variable("X"), new Variable("Y") }, new AddExpression(new VariableExpression(new Variable("X")), new VariableExpression(new Variable("Y"))));
+            module.Context.SetValue("add/2", function);
+            QualifiedCallExpression expr = new QualifiedCallExpression(new AtomExpression(new Atom("mod")), new AtomExpression(new Atom("add")), new IExpression[] { new ConstantExpression(1), new ConstantExpression(2) });
+
+            Assert.IsFalse(expr.HasVariable());
+            Assert.IsNotNull(expr.ModuleExpression);
+            Assert.IsInstanceOfType(expr.ModuleExpression, typeof(AtomExpression));
+            Assert.IsNotNull(expr.NameExpression);
+            Assert.IsInstanceOfType(expr.NameExpression, typeof(AtomExpression));
+            Assert.IsNotNull(expr.ArgumentExpressions);
+            Assert.AreEqual(2, expr.ArgumentExpressions.Count);
+
+            Assert.AreEqual(3, expr.Evaluate(context));
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/QualifiedCallExpression.cs
@@ -0,0 +1,59 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class QualifiedCallExpression : IExpression
+    {
+        private IExpression moduleexpression;
+        private IExpression nameexpression;
+        private IList<IExpression> argumentexpressions;
+
+        public QualifiedCallExpression(IExpression moduleexpression, IExpression nameexpression, IList<IExpression> argumentexpressions)
+        {
+            this.moduleexpression = moduleexpression;
+            this.nameexpression = nameexpression;
+            this.argumentexpressions = argumentexpressions;
+        }
+
+        public IExpression ModuleExpression { get { return this.moduleexpression; } }
+
+        public IExpression NameExpression { get { return this.nameexpression; } }
+
+        public IList<IExpression> ArgumentExpressions { get { return this.argumentexpressions; } }
+
+        public object Evaluate(Context context, bool withvars = false)
+        {
+            object modulevalue = this.moduleexpression.Evaluate(context, withvars);
+            object namevalue = this.nameexpression.Evaluate(context, withvars);
+
+            string modulename = ((Atom)modulevalue).Name;
+            string name = string.Format("{0}/{1}", ((Atom)namevalue).Name, this.argumentexpressions.Count);
+
+            IList<object> arguments = new List<object>();
+
+            foreach (var argexpr in this.argumentexpressions)
+                arguments.Add(argexpr.Evaluate(context, withvars));
+
+            Module module = (Module)context.GetValue(modulename);
+            IFunction func = (IFunction)module.Context.GetValue(name);
+
+            return func.Apply(context, arguments);
+        }
+
+        public bool HasVariable()
+        {
+            if (this.nameexpression.HasVariable())
+                return true;
+
+            foreach (var expr in this.argumentexpressions)
+                if (expr.HasVariable())
+                    return true;
+
+            return false;
+        }
+    }
+}
