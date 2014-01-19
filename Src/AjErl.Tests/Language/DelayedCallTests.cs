ef2ebf8ef1b7d5c6e541380dc3a7d94e74562da7
ajlopez <webmaster@ajlopez.com>
2014-01-19 18:54:01 -0300

    Delayed Call, first test

---
+++ Src/AjErl.Tests/Language/DelayedCallTests.cs
@@ -0,0 +1,39 @@
+﻿namespace AjErl.Tests.Language
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Language;
+    using AjErl.Compiler;
+
+    [TestClass]
+    public class DelayedCallTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateDelayedCall()
+        {
+            IFunction function = this.MakeFunction("f(X) -> X+1.");
+            Context context = new Context();
+            var arguments = new object[] { 1 };
+
+            DelayedCall dcall = new DelayedCall(function, context, arguments);
+
+            Assert.IsNotNull(dcall.Function);
+            Assert.AreSame(function, dcall.Function);
+            Assert.AreSame(context, dcall.Context);
+            Assert.AreSame(arguments, dcall.Arguments);
+
+            Assert.AreEqual(2, dcall.Evaluate());
+        }
+
+        private IFunction MakeFunction(string text)
+        {
+            Context context = new Context();
+            Parser parser = new Parser(text);
+            var form = parser.ParseForm();
+            return (IFunction)form.Evaluate(context);
+        }
+    }
+}
---
+++ Src/AjErl/Language/DelayedCall.cs
@@ -0,0 +1,32 @@
+﻿namespace AjErl.Language
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class DelayedCall
+    {
+        private IFunction function;
+        private Context context;
+        private IList<object> arguments;
+
+        public DelayedCall(IFunction function, Context context, IList<object> arguments)
+        {
+            this.function = function;
+            this.context = context;
+            this.arguments = arguments;
+        }
+
+        public IFunction Function { get { return this.function; } }
+
+        public Context Context { get { return this.context; } }
+
+        public IList<object> Arguments { get { return this.arguments; } }
+
+        public object Evaluate()
+        {
+            return this.function.Apply(context, arguments);
+        }
+    }
+}
