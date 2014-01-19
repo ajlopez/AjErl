5ec997a36a70aa95fb2649eec9744c6b77eed585
ajlopez <webmaster@ajlopez.com>
2014-01-19 18:13:21 -0300

    CompositeExpression with first tests

---
+++ Src/AjErl.Tests/Expressions/CompositeExpressionTests.cs
@@ -0,0 +1,55 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Expressions;
+    using AjErl.Language;
+
+    [TestClass]
+    public class CompositeExpressionTests
+    {
+        [TestMethod]
+        public void CreateAndEvaluateCompositeExpressionWithTwoConstants()
+        {
+            IExpression expr1 = new ConstantExpression(1);
+            IExpression expr2 = new ConstantExpression(2);
+
+            CompositeExpression expr = new CompositeExpression(new IExpression[] { expr1, expr2 });
+
+            Assert.IsFalse(expr.HasVariable());
+            Assert.IsNotNull(expr.Expressions);
+            Assert.AreEqual(2, expr.Expressions.Count);
+            Assert.AreSame(expr1, expr.Expressions[0]);
+            Assert.AreSame(expr2, expr.Expressions[1]);
+
+            var result = expr.Evaluate(null);
+
+            Assert.AreEqual(2, result);
+        }
+
+        [TestMethod]
+        public void CreateAndEvaluateCompositeExpressionWithTwoVariables()
+        {
+            IExpression expr1 = new VariableExpression(new Variable("X"));
+            IExpression expr2 = new VariableExpression(new Variable("Y"));
+            Context context = new Context();
+            context.SetValue("X", 1);
+            context.SetValue("Y", 2);
+
+            CompositeExpression expr = new CompositeExpression(new IExpression[] { expr1, expr2 });
+
+            Assert.IsTrue(expr.HasVariable());
+            Assert.IsNotNull(expr.Expressions);
+            Assert.AreEqual(2, expr.Expressions.Count);
+            Assert.AreSame(expr1, expr.Expressions[0]);
+            Assert.AreSame(expr2, expr.Expressions[1]);
+
+            var result = expr.Evaluate(context);
+
+            Assert.AreEqual(2, result);
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/CompositeExpression.cs
@@ -0,0 +1,38 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class CompositeExpression : IExpression
+    {
+        private IList<IExpression> expressions;
+
+        public CompositeExpression(IList<IExpression> expressions)
+        {
+            this.expressions = expressions;
+        }
+
+        public IList<IExpression> Expressions { get { return this.expressions; } }
+
+        public object Evaluate(Context context, bool withvars = false)
+        {
+            object result = null;
+
+            foreach (var expr in this.expressions)
+                result = expr.Evaluate(context, withvars);
+
+            return result;
+        }
+
+        public bool HasVariable()
+        {
+            foreach (var expr in this.expressions)
+                if (expr.HasVariable())
+                    return true;
+
+            return false;
+        }
+    }
+}
