9b3aa4d106bc8c1ea30f0ef648715ab809f5eed2
ajlopez <webmaster@ajlopez.com>
2014-01-26 16:42:46 -0300

    MultiFunExpression with first tests

---
+++ Src/AjErl.Tests/Expressions/MultiFunExpressionTests.cs
@@ -0,0 +1,80 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Language;
+    using AjErl.Compiler;
+    using AjErl.Expressions;
+
+    [TestClass]
+    public class MultiFunExpressionTests
+    {
+        [TestMethod]
+        public void EvaluateWithOneExpression()
+        {
+            Context context = new Context();
+            FunExpression expr = this.MakeExpression("fun(0) -> 1 end.");
+            MultiFunExpression mexpr = new MultiFunExpression(new FunExpression[] { expr });
+
+            var result = mexpr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(MultiFunction));
+
+            var mfunc = (MultiFunction)result;
+
+            Assert.IsNotNull(mfunc.Functions);
+            Assert.AreEqual(1, mfunc.Functions.Count);
+        }
+
+        [TestMethod]
+        public void EvaluateWithThreeExpressions()
+        {
+            Context context = new Context();
+            FunExpression expr1 = this.MakeExpression("fun(0) -> 1 end.");
+            FunExpression expr2 = this.MakeExpression("fun(1) -> 1 end.");
+            FunExpression expr3 = this.MakeExpression("fun(X) -> f(X-1) + f(X-2) end.");
+            MultiFunExpression mexpr = new MultiFunExpression(new FunExpression[] { expr1, expr2, expr3 });
+
+            var result = mexpr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(MultiFunction));
+
+            var mfunc = (MultiFunction)result;
+
+            Assert.IsNotNull(mfunc.Functions);
+            Assert.AreEqual(3, mfunc.Functions.Count);
+        }
+
+        [TestMethod]
+        public void RaiseIfArityIsWrong()
+        {
+            Context context = new Context();
+            FunExpression expr1 = this.MakeExpression("fun(0) -> 1 end.");
+            FunExpression expr2 = this.MakeExpression("fun(1) -> 1 end.");
+            FunExpression expr3 = this.MakeExpression("fun(X, Y) -> f(X-1) + f(X-2) end.");
+            MultiFunExpression mexpr = new MultiFunExpression(new FunExpression[] { expr1, expr2, expr3 });
+
+            try
+            {
+                mexpr.Evaluate(context);
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("head mismatch", ex.Message);
+            }
+        }
+
+        private FunExpression MakeExpression(string text)
+        {
+            Context context = new Context();
+            Parser parser = new Parser(text);
+            return (FunExpression)parser.ParseExpression();
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/MultiFunExpression.cs
@@ -0,0 +1,42 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class MultiFunExpression : IExpression
+    {
+        private IList<FunExpression> expressions;
+
+        public MultiFunExpression(IList<FunExpression> expressions)
+        {
+            this.expressions = expressions;
+        }
+
+        public IList<FunExpression> Expressions { get { return this.expressions; } }
+
+        public object Evaluate(Context context, bool withvars = false)
+        {
+            int arity = this.expressions[0].ParameterExpressions.Count;
+
+            if (!this.expressions.Skip(1).All(f => f.ParameterExpressions.Count == arity))
+                throw new Exception("head mismatch");
+
+            IList<Function> functions = new List<Function>();
+
+            foreach (var form in this.Expressions)
+                functions.Add((Function)form.Evaluate(context, true));
+
+            var func = new MultiFunction(functions);
+
+            return func;
+        }
+
+        public bool HasVariable()
+        {
+            return false;
+        }
+    }
+}
