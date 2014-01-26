a512f6e62781c169b0b25d819c21c43c0dec86e5
ajlopez <webmaster@ajlopez.com>
2014-01-26 16:02:43 -0300

    FunExpression first test

---
+++ Src/AjErl.Tests/Expressions/FunExpressionTests.cs
@@ -0,0 +1,36 @@
+﻿namespace AjErl.Tests.Expressions
+{
+    using System;
+    using System.Text;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using AjErl.Expressions;
+    using AjErl.Language;
+
+    [TestClass]
+    public class FunExpressionTests
+    {
+        [TestMethod]
+        public void DefineAndEvaluateFunction()
+        {
+            FunExpression expr = new FunExpression(new IExpression[] { new VariableExpression(new Variable("X")), new VariableExpression(new Variable("Y")) }, new AddExpression(new VariableExpression(new Variable("X")), new VariableExpression(new Variable("Y"))));
+            Context context = new Context();
+
+            var result = expr.Evaluate(context);
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(Function));
+
+            var func = (Function)result;
+
+            var newcontext = func.MakeContext(new object[] { 1, 2 });
+
+            Assert.IsNotNull(newcontext);
+            Assert.AreEqual(1, newcontext.GetValue("X"));
+            Assert.AreEqual(2, newcontext.GetValue("Y"));
+
+            Assert.AreEqual(3, func.Evaluate(newcontext));
+        }
+    }
+}
---
+++ Src/AjErl/Expressions/FunExpression.cs
@@ -0,0 +1,53 @@
+﻿namespace AjErl.Expressions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Language;
+
+    public class FunExpression : IExpression
+    {
+        private IList<IExpression> parameterexpressions;
+        private IExpression body;
+
+        public FunExpression(IList<IExpression> parameterexpressions, IExpression body)
+        {
+            this.parameterexpressions = parameterexpressions;
+            this.body = body;
+
+            if (body is CallExpression)
+                this.body = ((CallExpression)body).ToDelayedCallExpression();
+            else if (body is CompositeExpression)
+            {
+                var cexpr = (CompositeExpression)body;
+                var last = cexpr.Expressions.Count - 1;
+
+                if (cexpr.Expressions[last] is CallExpression)
+                    cexpr.Expressions[last] = ((CallExpression)cexpr.Expressions[last]).ToDelayedCallExpression();
+            }
+        }
+
+        public IList<IExpression> ParameterExpressions { get { return this.parameterexpressions; } }
+
+        public IExpression Body { get { return this.body; } }
+
+        public object Evaluate(Context context, bool withvars = false)
+        {
+            Context newcontext = new Context();
+            IList<object> parameters = new List<object>();
+
+            foreach (var pexpr in this.parameterexpressions)
+                parameters.Add(pexpr.Evaluate(newcontext, true));
+
+            var func = new Function(context, parameters, this.body);
+
+            return func;
+        }
+
+        public bool HasVariable()
+        {
+            return false;
+        }
+    }
+}
