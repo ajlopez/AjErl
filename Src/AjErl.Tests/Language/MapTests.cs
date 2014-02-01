f93c4fa32f902cb2cdb5ec44d5244d7fd584b870
ajlopez <webmaster@ajlopez.com>
2014-02-01 19:43:32 -0300

    Map.SetKeyValues with first test

--- Src/AjErl.Tests/Language/MapTests.cs
+++ Src/AjErl.Tests/Language/MapTests.cs
@@ -20,6 +20,22 @@
         }
 
         [TestMethod]
+        public void SetKeyValues()
+        {
+            Map map = new Map(new object[] { new Atom("a"), new Atom("b"), new Atom("c") }, new object[] { 1, 2, 3 });
+
+            Map newmap = map.SetKeyValues(new object[] { new Atom("c"), new Atom("a") }, new object[] { 4, 5 });
+
+            Assert.AreEqual(1, map.GetValue(new Atom("a")));
+            Assert.AreEqual(2, map.GetValue(new Atom("b")));
+            Assert.AreEqual(3, map.GetValue(new Atom("c")));
+
+            Assert.AreEqual(5, newmap.GetValue(new Atom("a")));
+            Assert.AreEqual(2, newmap.GetValue(new Atom("b")));
+            Assert.AreEqual(4, newmap.GetValue(new Atom("c")));
+        }
+
+        [TestMethod]
         public void RaiseIfUndefinedKey()
         {
             Map map = new Map(new object[] { new Atom("a"), new Atom("b") }, new object[] { 1, 2 });
--- Src/AjErl/Language/Map.cs
+++ Src/AjErl/Language/Map.cs
@@ -16,6 +16,12 @@
             this.values = values.ToArray();
         }
 
+        private Map(object[] keys, object[] values)
+        {
+            this.keys = keys;
+            this.values = values;
+        }
+
         public object GetValue(object key)
         {
             int position = ((IList<object>)keys).IndexOf(key);
@@ -24,6 +30,21 @@
                 throw new InvalidOperationException(string.Format("undefined key {0}", key));
 
             return values[position];
+        }
+
+        public Map SetKeyValues(IList<object> keys, IList<object> newvalues)
+        {
+            object[] newvals = new object[this.values.Length];
+            Array.Copy(this.values, newvals, this.values.Length);
+
+            for (int k = 0; k < keys.Count; k++)
+            {
+                object key = keys[k];
+                int position = ((IList<object>)this.keys).IndexOf(key);
+                newvals[position] = newvalues[k];
+            }
+
+            return new Map(this.keys, newvals);
         }
     }
 }
