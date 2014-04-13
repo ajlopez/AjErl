7d5d8b300eaf93be6537bcaad50d2dae21099639
ajlopez <webmaster@ajlopez.com>
2014-04-13 07:14:15 -0300

    InputChannel with first tests

---
+++ Src/AjErl.Tests/Communication/InputChannelTests.cs
@@ -0,0 +1,61 @@
+﻿namespace AjErl.Tests.Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using AjErl.Communication;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class InputChannelTests
+    {
+        [TestMethod]
+        public void ReadNull()
+        {
+            MemoryStream stream = new MemoryStream();
+            OutputChannel output = new OutputChannel(new BinaryWriter(stream));
+            output.Write(null);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            InputChannel channel = new InputChannel(new BinaryReader(stream));
+
+            Assert.IsNull(channel.Read());
+        }
+
+        [TestMethod]
+        public void ReadInteger()
+        {
+            MemoryStream stream = new MemoryStream();
+            OutputChannel output = new OutputChannel(new BinaryWriter(stream));
+            output.Write(123);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            InputChannel channel = new InputChannel(new BinaryReader(stream));
+
+            Assert.AreEqual(123, channel.Read());
+        }
+
+        [TestMethod]
+        public void ReadInvalidData()
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryWriter writer = new BinaryWriter(stream);
+            writer.Write((byte)255);
+            stream.Seek(0, SeekOrigin.Begin);
+
+            InputChannel channel = new InputChannel(new BinaryReader(stream));
+
+            try
+            {
+                channel.Read();
+                Assert.Fail();
+            }
+            catch (Exception ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(InvalidDataException));
+            }
+        }
+    }
+}
---
+++ Src/AjErl/Communication/InputChannel.cs
@@ -0,0 +1,33 @@
+﻿namespace AjErl.Communication
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class InputChannel
+    {
+        private BinaryReader reader;
+
+        public InputChannel(BinaryReader reader)
+        {
+            this.reader = reader;
+        }
+
+        public object Read()
+        {
+            byte type = this.reader.ReadByte();
+
+            switch (type)
+            {
+                case (byte)Types.Null:
+                    return null;
+                case (byte)Types.Integer:
+                    return this.reader.ReadInt32();
+            }
+
+            throw new InvalidDataException();
+        }
+    }
+}
